dd8529dad5847e3a7f24e4ce42cdc180495a0414
link-forte <s2.tsukimi+github@gmail.com>
2022-06-14 00:32:41 +0900

    add sample

--- js/index.js
+++ js/index.js
@@ -1,6 +1,42 @@
-const app = new Vue ({
-    el: '#app',
-    data: {
-        title: 'hoge',
-    }
-})
+(() => {
+    const app = new Vue ({
+        el: '#app',
+
+        data () {
+            return {
+                title: 'hoge',
+                count: 0,
+                goal_flag: false,
+            }
+        },
+        methods: {
+            up: () => {
+                app.count++;
+            },
+            down: () => {
+                app.count--;
+            },
+        },
+        computed: {
+            checkCount () {
+                return this.count >= 10 ? 'OK' : 'NG';
+            },
+        },
+        watch: {
+            count(val) {
+                if (val >= 15) {
+                    this.goal_flag = true;
+                } else if (val < 15) {
+                    this.goal_flag = false;
+                }
+            },
+        },
+
+        mounted () {
+            this.$nextTick(function () {
+                console.log('mounted');
+            })
+        },
+    });
+})();
+
